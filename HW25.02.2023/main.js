772603a145eb44ea0990a3cc050c0ac7d7e13990
YuliiaShulimenko <shulichka4@gmail.com>
2023-03-19 17:36:18 +0100

    Figma

--- HW25.02.2023/main.js
+++ HW25.02.2023/main.js
@@ -1,46 +1,44 @@
-// ****😢😢😢*******Тигран,я честно 2 недели каждый день сидела над этим сайтом,у меня то там,то сям вечно что-то ломается,верстка куда-то ползет вечно,сделаю кнопку,не работает точка,сделаю точку,не крутит слайд.В общем у меня просто уже нервов и сил не хватает доделать адаптив,глаз замылен и ошибок не вижу
-//ПОэтому сделала столько,на сколько моих сил и нервов хватила,не судите строго
+// // ****😢😢😢*******Тигран,я честно 2 недели каждый день сидела над этим сайтом,у меня то там,то сям вечно что-то ломается,верстка куда-то ползет вечно,сделаю кнопку,не работает точка,сделаю точку,не крутит слайд.В общем у меня просто уже нервов и сил не хватает доделать адаптив,глаз замылен и ошибок не вижу
+// //ПОэтому сделала столько,на сколько моих сил и нервов хватила,не судите строго
 
-// Не получается:
+// // Не получается:
+// Модальное окно тоже как-то криво работает
+// // -Див майкрасофт где*клиенты) по стрелке не листает ВПРАВО,работает только влево
 
-// -не сделала открывающееся окно меню(мобильное окно меню)_Т.Е. СДЕЛАЛА,НО ПРИШЛОСЬ ЗАКОММЕНТИТЬ,ТК ОНО СРАЗУ ВЫЛАЗИТ И ЧТОБЫ ЕГО УБРАТЬ,МНЕ НУЖНО СУЗИТЬ ЭКРАН ДО МЕДИА ЗАПРОСА С ПОЯВЛЕЕМ БУРГЕРА И УБРАТЬ ЭТО МОДАЛЬНОЕ ОКНО,ХОТЯ ИЗНАЧАЛЬНО ЗАДАЮ ЕМУ ДИСПЛЕЙ НАН
-// -Див майкрасофт где*клиенты) по стрелке не листает
-
-//-на последнем слайде с отзывами вроде слайдер работает,но если я влево жму стрелочку и оно пролистывается в обратную сторону дальше 1-й точки,то оно куда-то вылазит и экран белый становится
+// //-на последнем слайде с отзывами вроде слайдер работает,но если я влево жму стрелочку и оно пролистывается в обратную сторону дальше 1-й точки,то оно куда-то вылазит и экран белый становится
 
 
-//я не знаю как исправить эти ошибки и довести до ума,пыталась написать одну универсальную фунцию для трех слайдов,обязательно что-то где-то ломается
+// //я не знаю как исправить эти ошибки и довести до ума,пыталась написать одну универсальную фунцию для трех слайдов,обязательно что-то где-то
 
-// Задана ссылка на figma-макет:
-// https://www.figma.com/file/AnxdgZrMkMxfNcDMl13Rhi/Template?node-id=0%3A1&t=zq6TBZXoeVBLObwE-0
+// // Задана ссылка на figma-макет:
+// // https://www.figma.com/file/AnxdgZrMkMxfNcDMl13Rhi/Template?node-id=0%3A1&t=zq6TBZXoeVBLObwE-0
 
-// Задача: сверстать страницу, учитывая расположение и позиционирование элементов согласно макету.
+// // Задача: сверстать страницу, учитывая расположение и позиционирование элементов согласно макету.
 
-// Требования к работе:
-// Наличие семантических тегов
-// Количество заголовков (H1) на странице не должно превышать единицы
-// У каждой используемой картинки должна быть задана ширина/высота
-// В макете упоминается слайдер. Его необходимо реализовать на стороне JS. В качестве 2,3,4 элемента слайда необходимо взять 1 картинку (Референс: макет).
-// Страница должна быть адаптивной (Референс: макет).
-// Для разрешение (ширина - 800px) необходимо реализовать бургер меню с выпадающим модальным окном (Референс: макет).
-
+// // Требования к работе:
+// // Наличие семантических тегов
+// // Количество заголовков (H1) на странице не должно превышать единицы
+// // У каждой используемой картинки должна быть задана ширина/высота
+// // В макете упоминается слайдер. Его необходимо реализовать на стороне JS. В качестве 2,3,4 элемента слайда необходимо взять 1 картинку (Референс: макет).
+// // Страница должна быть адаптивной (Референс: макет).
+// // Для разрешение (ширина - 800px) необходимо реализовать бургер меню с выпадающим модальным окном (Референс: макет).
 
 
 
 
-// 888888b.                                               
-// 888  "88b                                              
-// 888  .88P                                              
-// 8888888K.   8888b.  88888b.  88888b.   .d88b.  888d888 
-// 888  "Y88b     "88b 888 "88b 888 "88b d8P  Y8b 888P"   
-// 888    888 .d888888 888  888 888  888 88888888 888     
-// 888   d88P 888  888 888  888 888  888 Y8b.     888     
-// 8888888P"  "Y888888 888  888 888  888  "Y8888  888     
+
+// // 888888b.                                               
+// // 888  "88b                                              
+// // 888  .88P                                              
+// // 8888888K.   8888b.  88888b.  88888b.   .d88b.  888d888 
+// // 888  "Y88b     "88b 888 "88b 888 "88b d8P  Y8b 888P"   
+// // 888    888 .d888888 888  888 888  888 88888888 888     
+// // 888   d88P 888  888 888  888 888  888 Y8b.     888     
+// // 8888888P"  "Y888888 888  888 888  888  "Y8888  888     
 
 // создаем массив для верхнего слайда,где будут меняться кнопки и текст
 let slider_index = 0;
-
-const banner = [
+ const banner = [
   {
     id: 1,
     title: "Бухгалтерские услуги в вашем городе",
@@ -140,128 +138,123 @@ function changeRounds(slider_index) {
 
 
 
-//   .d8888b.  888 d8b                   888             
-//   d88P  Y88b 888 Y8P                   888             
-//   888    888 888                       888             
-//   888        888 888  .d88b.  88888b.  888888 .d8888b  
-//   888        888 888 d8P  Y8b 888 "88b 888    88K      
-//   888    888 888 888 88888888 888  888 888    "Y8888b. 
-//   Y88b  d88P 888 888 Y8b.     888  888 Y88b.       X88 
-//    "Y8888P"  888 888  "Y8888  888  888  "Y888  88888P' 
+// //   .d8888b.  888 d8b                   888             
+// //   d88P  Y88b 888 Y8P                   888             
+// //   888    888 888                       888             
+// //   888        888 888  .d88b.  88888b.  888888 .d8888b  
+// //   888        888 888 d8P  Y8b 888 "88b 888    88K      
+// //   888    888 888 888 88888888 888  888 888    "Y8888b. 
+// //   Y88b  d88P 888 888 Y8b.     888  888 Y88b.       X88 
+// //    "Y8888P"  888 888  "Y8888  888  888  "Y888  88888P' 
              
+const arrowsLeft = document.querySelector('.clients_arrow_left');
+const arrowsRight = document.querySelector('.clients_arrow_right');
+const rounds = document.querySelectorAll('.clients_round');
+const slider = document.querySelector('.clientsList');
+const slidesWidth = 1100;
+let counter = 0;
 
+arrowsLeft.addEventListener('click', function() {
+	if(counter === 0) {
+		counter = rounds.length -1;
+		slider.style.transform = `translateX(${-slidesWidth * counter}px)`;
+	} else {
+		counter--;
+		slider.style.transform = `translateX(${-slidesWidth *counter}px)`;
+	}
+	selectRound();
+});
 
+arrowsRight.addEventListener('click', function() {
+	if(counter >= rounds.length - 1) {
+		counter = 0;
+		slider.style.transform = `translateX(${-slidesWidth * counter}px)`;
+	 } else {
+		counter++;
+		slider.style.transform = `translateX(${-slidesWidth * counter}px)`;
+	}
+	selectRound();
+});
 
-
-//У меня уже глаза замылились...Тут работает только при нажатии на кружочки
-
-
-const clients_arrow_left = document.querySelector('.clients_arrow_left');
-const clients_arrow_right = document.querySelector('.clients_arrow_right');
-const clients_round = document.querySelectorAll('.clients_round');
-const clientsList = document.querySelector('.clientsList');
-let activePos = 0;
-
-for (let i = 0; i < clients_round.length; i++) {
-  clients_round[i].addEventListener('click', function () {
-    activePos = i;
-    let currentMarginLeft = activePos * -1100;
-    clientsList.style.marginLeft = `${currentMarginLeft}px`;
-    toggleRoundClass(activePos);
-  });
+for(let i = 0; i < rounds.length; i++) {
+	rounds[i].addEventListener('click', function(){
+		counter = i;
+		slider.style.transform = `translateX(${-slidesWidth * counter}px)`;
+		selectRound();
+	});
 }
-clients_arrow_left.addEventListener('click', () => {
-  activePos--;
-  if (activePos < 0) {
-    activePos = 0;
-  }
-  let currentMarginLeft = activePos * -1100;
-  clientsList.style.marginLeft = `${currentMarginLeft}px`;
-  toggleRoundClass(activePos);
-});
-clients_arrow_right.addEventListener('click', () => {
-  activePos++;
-  if (activePos > clients_round.length - 1) {
-    activePos = clients_round.length - 1;
-  }
-  let currentMarginLeft = activePos * -1100;
-  clientsList.style.marginLeft = `${currentMarginLeft}px`;
-  toggleRoundClass(activePos);
-});
-function toggleRoundClass(activePos) {
-  clients_round.forEach(function (item, index) {
-    item.classList.remove('clients_round_active');
-    if (activePos === index) {
-      item.classList.add('clients_round_active');
-    }
-  });
+
+function selectRound(){
+	for(let i = 0; i < rounds.length; i++) {
+		rounds[i].classList.remove('clients_round_active');
+	}
+	rounds[counter].classList.add('clients_round_active');
 }
 
 
 
-
-// 8888888b.                   d8b                                 
-// 888   Y88b                  Y8P                                 
-// 888    888                                                      
-// 888   d88P .d88b.  888  888 888  .d88b.  888  888  888 .d8888b  
-// 8888888P" d8P  Y8b 888  888 888 d8P  Y8b 888  888  888 88K      
-// 888 T88b  88888888 Y88  88P 888 88888888 888  888  888 "Y8888b. 
-// 888  T88b Y8b.      Y8bd8P  888 Y8b.     Y88b 888 d88P      X88 
-// 888   T88b "Y8888    Y88P   888  "Y8888   "Y8888888P"   88888P'
-
-
-
-const slides = document.querySelectorAll('.reviews_slide'),
-      round  = document.querySelectorAll('.reviews_round'),
-      prev   = document.querySelector('.reviews_arrow_left'),
-      next   = document.querySelector('.reviews_arrow_right'),
-      slider = document.querySelector('.reviews_frame');
-
-let slideIndex = 1;
-
-const activeDot = () => {
-  round.forEach(item => item.classList.remove('reviews_round_active'));
-  round[slideIndex - 1].classList.add('reviews_round_active');
-}
-
-const activeSlide = (n) => {
-  slideIndex += n;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  if (slideIndex < 1) {
-    slideIndex = slides.length;
-  }
-  slider.style.transform = `translateX(${-slideIndex * 1110}px)`;
-  activeDot();
-}
-
-prev.addEventListener('click', function() {
-  activeSlide(-1);
-});
-next.addEventListener('click', function() {
-  activeSlide(1);
-});
-
-round.forEach((item, index) => {
-  item.addEventListener('click', function() {
-    slideIndex = index + 1;
-    slider.style.transform = `translateX(${-slideIndex * 1110}px)`;
-    activeDot();
-  });
-});
+// // 8888888b.                   d8b                                 
+// // 888   Y88b                  Y8P                                 
+// // 888    888                                                      
+// // 888   d88P .d88b.  888  888 888  .d88b.  888  888  888 .d8888b  
+// // 8888888P" d8P  Y8b 888  888 888 d8P  Y8b 888  888  888 88K      
+// // 888 T88b  88888888 Y88  88P 888 88888888 888  888  888 "Y8888b. 
+// // 888  T88b Y8b.      Y8bd8P  888 Y8b.     Y88b 888 d88P      X88 
+// // 888   T88b "Y8888    Y88P   888  "Y8888   "Y8888888P"   88888P'
 
 
 
-//МОДАЛЬНОЕ ОКНО ПРИШЛОСЬ ЗАКОМЕНТИРОВАТЬ,ТК ХОТЯ Я ЕМУ ВЫСТАВИЛА ИЗНАЧАЛЬНО ДИСПЛЕЙ НАН,ТО ОНО У МЕНЯ ОТОБРАЖАЛОСЬ СРАЗУ
-//-----------------
+// const slides = document.querySelectorAll('.reviews_slide')
+// const round  = document.querySelectorAll('.reviews_round')
+// const prev   = document.querySelector('.reviews_arrow_left')
+// const next   = document.querySelector('.reviews_arrow_right')
+// const slider = document.querySelector('.reviews_frame');
 
-// const burgerBtn =document.querySelector(".burger")
-// const modal = document.querySelector(".modal")
-// let showModal = false
+// let slideIndex = 0;
 
-// burgerBtn.onclick=()=>{
-//   showModal = !showModal
-//   showModal ? modal.style.display= "flex" : modal.style.display= "none"
-//   console.log(showModal)
+// const activeDot = () => {
+//   round.forEach(item => item.classList.remove('reviews_round_active'));
+//   round[slideIndex - 1].classList.add('reviews_round_active');
 // }
+
+// const activeSlide = (n) => {
+//   slideIndex += n;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (slideIndex < 1) {
+//     slideIndex = slides.length;
+//   }
+//   slider.style.transform = `translateX(${-slideIndex * 1110}px)`;
+//   activeDot();
+// }
+
+// prev.addEventListener('click', function() {
+//   activeSlide(-1);
+// });
+// next.addEventListener('click', function() {
+//   activeSlide(1);
+// });
+
+// round.forEach((item, index) => {
+//   item.addEventListener('click', function() {
+//     slideIndex = index + 1;
+//     slider.style.transform = `translateX(${-slideIndex * 1110}px)`;
+//     activeDot();
+//   });
+// });
+
+
+
+// //МОДАЛЬНОЕ ОКНО 
+// //-----------------
+
+const burgerBtn =document.querySelector(".burger")
+const modal = document.querySelector(".modal")
+let showModal = false
+
+burgerBtn.onclick=()=>{
+  showModal = !showModal
+  showModal ? modal.style.display= "flex" : modal.style.display= "none"
+  console.log(showModal)
+}
